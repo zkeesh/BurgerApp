0d934cf00ad5478f3308b5a9e261180a40d54c1a
Zachary <zkeesh@gmail.com>
2017-06-03 17:48:35 -0500

    added ORM

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,20 @@
 var orm = require('../config/orm.js');
+
+var burger = {
+  selectAll: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
